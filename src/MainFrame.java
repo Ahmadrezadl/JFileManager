312b33730a38a8a2ef0860840078132022f9bc8e
Ahmadrezadl <Ahmadrezakamali121@yahoo.com>
2019-11-29 21:42:58 +0330

    Create MainFrame.

---
+++ src/MainFrame.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+    public MainFrame(){
+        super("JFileManager");
+        this.setVisible(true);
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+    }
+}
